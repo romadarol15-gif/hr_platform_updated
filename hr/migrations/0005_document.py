6d8b71e7fec664f3357b04c5bb1ccbf2f32816be
romadarol15-gif <roma.darol15@gmail.com>
2026-02-22 18:48:13 +0300

    ДОБАВЛЕНА: миграция для модели Document
    Migration для создания таблицы hr_document с полями:
    - name (название)
    - comment (комментарий)
    - file (файл с валидацией 10МБ)
    - uploaded_at (дата загрузки)
    - employee (внешний ключ)

---
+++ hr/migrations/0005_document.py
@@ -0,0 +1,31 @@
+# Generated migration
+
+from django.db import migrations, models
+import django.db.models.deletion
+import hr.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hr', '0004_alter_employee_hire_date_positionhistory'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Document',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=200, verbose_name='Название')),
+                ('comment', models.TextField(blank=True, verbose_name='Комментарий')),
+                ('file', models.FileField(upload_to='employee_documents/', validators=[hr.models.validate_file_size], verbose_name='Файл')),
+                ('uploaded_at', models.DateTimeField(auto_now_add=True, verbose_name='Дата загрузки')),
+                ('employee', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='documents', to='hr.employee', verbose_name='Сотрудник')),
+            ],
+            options={
+                'verbose_name': 'Документ',
+                'verbose_name_plural': 'Документы',
+                'ordering': ['-uploaded_at'],
+            },
+        ),
+    ]
